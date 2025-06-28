6e60db45833833dd195158c8756fc90686842071
pablozamit <166626152+pablozamit@users.noreply.github.com>
2025-06-28 11:34:18 +0200

    Remove non-plugin files

--- src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/generate-divi-elements.ts';
-import '@/ai/flows/create-bulk-page-layout.ts';
-import '@/ai/flows/modify-divi-elements.ts';
